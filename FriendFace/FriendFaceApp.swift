557e903c5cc8e85b4562b9bfb3e8f9a903c36c39
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-13 23:50:18 -0300

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Matheus Viana on 13/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
