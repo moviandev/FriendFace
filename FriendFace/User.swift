bac8fb1bcf64c241ad6a39801d2b87ccff4b6b5e
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-14 23:38:25 -0300

    Uncomment registered and add company field

--- FriendFace/User.swift
+++ FriendFace/User.swift
@@ -11,11 +11,12 @@ struct User: Identifiable, Codable {
     let id: UUID
     let isActive: Bool
     let name: String
+    let company: String
     let age: Int
     let email: String
     let address: String
     let about: String
-//    let registered: Date
+    let registered: Date
     let tags: [String]
     let friends: [Friend]
     
