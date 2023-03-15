9876f21eeddb8fade154d8f9bfb2e9fb24534f6a
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-14 23:38:56 -0300

    add user information and make it a recursive view

--- FriendFace/UserView.swift
+++ FriendFace/UserView.swift
@@ -8,16 +8,63 @@
 import SwiftUI
 
 struct UserView: View {
-//    @State var users: User
+    var user: User
+    var users: [User]
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section("Registered in") {
+                Text("\(user.registered.formatted(date: .complete, time: .omitted))")
+            }
+            
+            Section("Age") {
+                Text("\(user.age) years old")
+            }
+            
+            Section("Works at") {
+                Text(user.company)
+            }
+            
+            Section("Email") {
+                Text(user.email)
+            }
+            
+            Section("Address") {
+                Text(user.address)
+            }
+            
+            Section("They like") {
+                ForEach(user.tags, id: \.self) { tag in
+                    Text(tag)
+                }
+            }
+            
+            Section("They are friend with") {
+                ForEach(user.friends) { friend in
+                    NavigationLink {
+                        UserView(user: users.first(where: {$0.id == friend.id}) ?? user, users: users)
+                    } label: {
+                        Text(friend.name)
+                            .font(.headline)
+                            .foregroundColor(.primary)
+                    }
+                }
+            }
+            
+            Section("About") {
+                Text(user.about)
+            }
+        }
+        .preferredColorScheme(.dark)
+        .navigationTitle(user.titleName)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct UserList_Previews: PreviewProvider {
+    static let user = User(id: UUID(), isActive: true, name: "Test", company: "test company", age: 20, email: "test@test", address: "test address", about: "test about", registered: Date.now, tags: ["test", "test"], friends: [Friend]())
+    
     static var previews: some View {
-        UserView()
+        UserView(user: user, users: [User]())
     }
 }
