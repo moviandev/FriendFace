2121b6d01b31d29d20806ac5a07207692d2050a0
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-15 23:58:06 -0300

    Import coreData and implement dataController class

--- FriendFace/DataController.swift
+++ FriendFace/DataController.swift
@@ -5,4 +5,17 @@
 //  Created by Matheus Viana on 15/03/23.
 //
 
+import CoreData
 import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "FriendFace")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
